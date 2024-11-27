83e245c0317c87e5469636be3c02dad607583711
aleksandra.bikbaeva <aleksandra.bikbaeva_axiomjdk.ru>
2024-11-27 18:13:08 +0300

    remove @Autowired import

--- src/main/java/com/example/openapidemo/OpenAPIConfiguration.java
+++ src/main/java/com/example/openapidemo/OpenAPIConfiguration.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
